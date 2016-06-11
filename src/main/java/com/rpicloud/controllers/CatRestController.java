0556515d1afd1fc2af8c307fd1e6381d2662b471
Kasper Nissen <kni@it-minds.dk>
2016-06-11 08:51:12 +0200

    cats added

--- src/main/java/com/rpicloud/controllers/CatRestController.java
+++ src/main/java/com/rpicloud/controllers/CatRestController.java
@@ -20,14 +20,16 @@ public class CatRestController {
     public ResponseEntity<List<Cat>> getAllCats(){
         List<Cat> col = new ArrayList<>();
         col.add(new Cat("Mr. Meow", "http://25.media.tumblr.com/tumblr_lzrltrAZFC1rncb5fo1_400.gif", "2", "Meeow!"));
-        col.add(new Cat("Mr. Meow1", "http://25.media.tumblr.com/tumblr_m3exc4YOR01r03eaxo1_400.gif", "2", "Meeow!"));
+        col.add(new Cat("Mr. Purrr", "http://25.media.tumblr.com/tumblr_m3exc4YOR01r03eaxo1_400.gif", "2", "Meeow!"));
         col.add(new Cat("Mr. Meow2", "http://27.media.tumblr.com/tumblr_llngvqDxGp1qaw6efo1_500.gif", "2", "Meeow!"));
-        //col.add(new Cat("Mrs Daisy", "http://www.catgifpage.com/gifs/310.gif", "4", "Meeow!"));
+        col.add(new Cat("Kitten", "http://i.giphy.com/GxN4ics7OlvsA.gif", "4", "Meeow!"));
+        col.add(new Cat("Mrs Daisy", "http://www.catgifpage.com/gifs/310.gif", "4", "Meeow!"));
+        col.add(new Cat("Star Wars Cats", "http://i.giphy.com/9gISqB3tncMmY.gif", "4", "Star Cats!"));
         return new ResponseEntity<>(col, HttpStatus.OK);
     }
 
     @RequestMapping("/cats/version")
     public ResponseEntity<String> version(){
-       return new ResponseEntity<>("v1", HttpStatus.OK);
+       return new ResponseEntity<>("v2", HttpStatus.OK);
     }
 }
